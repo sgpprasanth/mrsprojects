f66fe6f3c547bd4709b6cee63309089222c3d0a0
sgp <somisetty.prasanth@gmail.com>
2018-05-12 19:47:34 +0530

    integration version modified

--- src/readme.md
+++ src/readme.md
@@ -1,3 +1,4 @@
 hello 
 new feature will be created in test file 
 this is first version qa
+changes moved to integration
--- src/test.cpp
+++ src/test.cpp
@@ -4,3 +4,5 @@ progrm initiated
 Fnctional program
 Feature 1 will be implimented
 
+Integrating all featured to on release version1
+
